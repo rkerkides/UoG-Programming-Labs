d9c92cf3e61cc12d53df4d1cc51ef64112bc27b6
Renos Kerkides <rkerkides@gmail.com>
2023-09-27 11:55:14 +0100

    Complete week 2 exercise 2

---
+++ Week 2/Lab/src/main/java/org/example/BankAccount.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class BankAccount {
+    private Person person;
+    private int accountNumber;
+    private double balance;
+    private static int nextAccountNumber = 0;
+
+    public BankAccount(Person person) {
+        this.person = person;
+        this.accountNumber = nextAccountNumber;
+        this.balance = 0.0;
+        nextAccountNumber++;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void depositFunds(double fundsToDeposit) {
+        this.balance += fundsToDeposit;
+    }
+
+    public void withdrawFunds(double fundsToWithdraw) {
+        if (this.balance >= fundsToWithdraw) {
+            this.balance -= fundsToWithdraw;
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+    public void transferFunds(BankAccount receiver, double fundsToTransfer) {
+        if (this.balance >= fundsToTransfer) {
+            withdrawFunds(fundsToTransfer);
+            receiver.depositFunds(fundsToTransfer);
+        }
+        System.out.println("Insufficient funds!");
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "name='" + person.getName() + '\'' +
+                ", accountNumber=" + accountNumber +
+                ", balance=" + balance +
+                '}';
+    }
+}
--- Week 2/Lab/src/main/java/org/example/Main.java
+++ Week 2/Lab/src/main/java/org/example/Main.java
@@ -5,5 +5,18 @@ public class Main {
         var john = new Person("John", 24, 185);
         john.sayHello();
         System.out.println(john);
+
+        var bank1 = new BankAccount(john);
+        var bank2 = new BankAccount(new Person("Vanessa", 23, 156));
+
+        System.out.println(bank1.getAccountNumber() + " " + bank2.getAccountNumber());
+        bank1.depositFunds(5324.2);
+        System.out.println(bank1);
+        bank1.withdrawFunds(123.41);
+        System.out.println(bank1);
+
+        var bank3 = new BankAccount(john);
+        bank1.transferFunds(bank3, 4213.23);
+        System.out.println(bank3 + " " + bank1);
     }
 }
