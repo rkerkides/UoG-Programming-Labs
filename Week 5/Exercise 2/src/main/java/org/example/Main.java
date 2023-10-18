dff0606ae9e7d16a99225d157f7403b76d4932b2
Renos Kerkides <rkerkides@gmail.com>
2023-10-18 12:10:45 +0100

    Complete week 5 exercise 2 part 2

--- Week 5/Exercise 2/src/main/java/org/example/Main.java
+++ Week 5/Exercise 2/src/main/java/org/example/Main.java
@@ -10,6 +10,47 @@ public class Main {
     public static void main(String[] args) {
         FileReader fileReader = null;
         try {
+            String filePath = "src/main/resources/sums.txt";
+            fileReader = new FileReader(filePath);
+            Scanner scanner = new Scanner(fileReader);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] tokens = line.split(" ");
+                try {
+                    int firstNumber = Integer.parseInt(tokens[0]);
+                    String operator = tokens[1];
+                    int secondNumber = Integer.parseInt(tokens[2]);
+                    switch (operator) {
+                        case "+":
+                            int sum = firstNumber + secondNumber;
+                            System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
+                            break;
+                        case "-":
+                            int difference = firstNumber - secondNumber;
+                            System.out.println(firstNumber + " - " + secondNumber + " = " + difference);
+                            break;
+                        default:
+                            System.out.println("Wrong operator.");
+                            break;
+                    }
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void part1(FileReader fileReader) {
+        try {
             String filePath = "src/main/resources/dolphin.txt";
             fileReader = new FileReader(filePath);
             Scanner scanner = new Scanner(fileReader);
