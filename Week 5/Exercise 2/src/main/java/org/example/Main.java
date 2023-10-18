528a5157e19467b52373b43772c6cdbea0be59ae
Renos Kerkides <rkerkides@gmail.com>
2023-10-18 11:52:25 +0100

    Complete week 5 exercise 2, part 1

---
+++ Week 5/Exercise 2/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        FileReader fileReader = null;
+        try {
+            String filePath = "src/main/resources/dolphin.txt";
+            fileReader = new FileReader(filePath);
+            Scanner scanner = new Scanner(fileReader);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
