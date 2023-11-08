08879751d2f0456457f085776cd74ecf53333b84
Renos Kerkides <rkerkides@gmail.com>
2023-11-08 12:12:37 +0000

    Complete week 8 exercise 2, part 2

--- Week 8/Exercise2/src/main/java/org/example/Main.java
+++ Week 8/Exercise2/src/main/java/org/example/Main.java
@@ -7,6 +7,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+       var p1 = new Person("John", 30, 180);
+       var p2 = new Person("Jane", 25, 170);
+       var p3 = new Person("Jack", 5, 110);
+       var p4 = new Person("Jill", 3, 90);
+       ArrayList<Person> people = new ArrayList<>();
+       people.add(p1);
+       people.add(p2);
+       people.add(p3);
+       people.add(p4);
+
+       for (Person p : people) {
+           p.sayHello();
+       }
+
+       
+    }
+
+    public static void exercise2part1() {
         FileReader fr = null;
         ArrayList<String> arrayList1 = new ArrayList<>();
         try {
