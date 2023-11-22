9a9ff810251b639a9780c9308673530a75c1f8eb
Renos Kerkides <rkerkides@gmail.com>
2023-11-22 10:34:12 +0000

    Complete Week 10 Exercise 2

--- Week 10/src/main/java/org/example/Main.java
+++ Week 10/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {1, 2, 3};
         System.out.println(addUpArray(array, 0, array.length - 2));
+        System.out.println(fibonacciNumber(12));
     }
 
     public static int addUpArray(int[] array, int i, int j) {
@@ -11,5 +12,12 @@ public class Main {
             return array[i];
         }
         return array[i] + addUpArray(array, i + 1, j);
+    }
+
+    public static int fibonacciNumber(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibonacciNumber(n - 1) + fibonacciNumber(n - 2);
     }
 }
